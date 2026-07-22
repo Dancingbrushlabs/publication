d7e0241e6b2b3d403022a631054d7513aa76cf60
Dancingbrushlabs <qoongz@gmail.com>
2026-07-22 19:22:13 +0900

    Configure custom Pages domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  site: "https://dancingbrushlabs.github.io",
-  base: process.env.GITHUB_ACTIONS ? "/publication" : "/",
+  site: "https://choomboot.com",
+  base: "/",
 });
